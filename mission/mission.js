e2aa0863ba76e31a7ff4d1c75b533caa960d6613
cbecker-dev <bec16022@byui.edu>
2026-01-17 13:53:09 -0700

    mission 2

--- mission/mission.js
+++ mission/mission.js
@@ -6,13 +6,17 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.body.style.backgroundColor = '#111';
-        document.body.style.color = '#fff';
+        document.body.style.backgroundColor = '#333';
+        document.body.style.color = 'white';
+
+        document.querySelector('#school').style.color = '#6fa8dc';
 
         logo.setAttribute('src', 'images/byui-logo-white.png');
     } else {
         document.body.style.backgroundColor = "#fff";
         document.body.style.color = '#111'
+
+        document.querySelector('#school').style.color = '#00008B';
 
         logo.setAttribute('src', 'images/byui-logo.png');
     }
